9a8eb973321fefe3049482279c80c84c4c2a0afd
ttt Uneugatoies <t3u33t333@gmail.com>
2024-03-06 18:31:35 +0900

    commit 5

--- java/src/baekjoon/BJ10610.java
+++ java/src/baekjoon/BJ10610.java
@@ -1,21 +1,62 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+// This can be computed by utilizing the divisibility rule; reference about it if you don't know what it is.
+public class BJ10610 {
+
+    private static void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char[] entry = br.readLine().toCharArray();
+        Arrays.sort(entry);
+        int sum = 0;
+        for (char c : entry)
+            sum += c - '0';
+
+        StringBuilder sb = new StringBuilder();
+        if (entry[0] == '0' && sum % 3 == 0) {
+            for (int i = entry.length - 1; i >= 0; --i)
+                sb.append(entry[i]);
+        } else {
+            sb.append("-1");
+        }
+        sb.append('\n');
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// I didn't even finish this; there's no way this is the right solution.
+/*
 public class BJ10610 {
 
     private static class Permutation {
         int n, r;
         int[] curr;
         List<List<Integer>> result;
-        public Permutation(int n, int r) {
+        public Permutation(int n) {
             this.n = n;
+        }
+
+        public void setR(int r) {
             this.r = r;
             this.curr = new int[r];
             this.result = new ArrayList<>();
@@ -45,25 +86,83 @@ public class BJ10610 {
         }
     }
 
+
+
     private static void solve() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         Integer[] arr = new Integer[s.length()];
         for (int i = 0; i < arr.length; ++i)
             arr[i] = s.charAt(i) - '0';
-        Arrays.sort(arr, (i1, i2) -> Integer.compare(i2, i1));
+        Arrays.sort(arr);
 
-        while ()
-        s = "";
+        boolean flag = false;
+        String result;
+        String temp;
+        int r = 1;
+        Permutation t = new Permutation(arr.length);
+        t.generatePermutation()
+        while (!flag) {
+            temp = "";
+            for (int i = 0; i < arr.length; ++i)
+                temp += String.valueOf(arr[arr.length - i - 1]);
+            int tempint;
+            if (temp.length() > 6) {
+                tempint = Integer.parseInt(temp.substring(temp.length() - 6));
+            } else {
+                tempint = Integer.parseInt(temp);
+            }
 
+            if (tempint % 30 == 0) {
+                result = temp;
+                break;
+            } else {
+                r++;
+                t.setR(r);
+            }
+        }
+    }
 
+    private static void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < s.length(); ++i)
+            arr.add(s.charAt(i) - '0');
+        Collections.sort(arr);
 
+        boolean flag = false;
+        String result;
+        String temp;
+        int r = 1;
+        Permutation permutation = new Permutation(arr.size());
+
+        while (!flag) {
+            temp = "";
+            for (int i = 0; i < arr.length; ++i)
+                temp += String.valueOf(arr[arr.length - i - 1]);
+            int tempint;
+            if (temp.length() > 6) {
+                tempint = Integer.parseInt(temp.substring(temp.length() - 6));
+            } else {
+                tempint = Integer.parseInt(temp);
+            }
+
+            if (tempint % 30 == 0) {
+                result = temp;
+                break;
+            } else {
+                r++;
+                permutation.setR(r);
+            }
+        }
     }
 
 
     public static void main(String[] args) throws IOException {
-        solve();
+
     }
 
 
 }
+*/
