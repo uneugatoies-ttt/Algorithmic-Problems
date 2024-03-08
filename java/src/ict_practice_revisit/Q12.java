ad014e2b12d3a9a3eaaef962b58b62d61ef71d8e
ttt Uneugatoies <t3u33t333@gmail.com>
2024-03-08 20:49:23 +0900

    commit 7

--- java/src/ict_practice_revisit/Q12.java
+++ java/src/ict_practice_revisit/Q12.java
@@ -2,10 +2,8 @@ package ict_practice_revisit;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
-
-
 
 /*
     A few things must be noted.
@@ -15,22 +13,19 @@ import java.util.List;
         2) Do not assume that the first idea that has come to your mind is the best.
 
         Of course, if you're trying to solve the problem in some kind of competition with a severe time limit,
-        then you should be able to come up with the best idea at the first trial.
+        you should be able to come up with the best idea at the first trial.
 
         But, in many other situations where you're trying to solve real-life problems,
-        you must be able to assess your idea whether it is good enough; and if it isn't,
-        you should be able to find an alternative.
+        you must assess your idea to find out whether it is good enough; and if it isn't,
+        you should find an alternative.
+        To find the alternative, maybe you should see the problem from the opposite direction;
+        or, maybe you should demolish the whole frame of yours that has been built.
+        Whatever your approach might be, you have to keep doing the similar process
+        until you find the best one.
 
-        Maybe you should see the problem from the opposite direction;
-        or maybe you must demolish the whole frame of yours that has been built.
-
-
-        Besides, this will eventually help me to think the best idea at the first trial, anyway.
-
+        Besides, although this might seem so overwhelmingly time-consuming,
+        it will eventually help me to think the best idea at the first trial.
  */
-
-
-/*
 
 public class Q12 {
 
@@ -59,17 +54,51 @@ public class Q12 {
     }
 
     private boolean isPossibleToOperate(List<List<Integer>> res) {
-        for (int i = 0; i < res.size(); ++i) {
-            int x = res.get(i).get(0);
-            int y = res.get(i).get(0);
-            int pob = res.get(i).get(0);
+        for (List<Integer> eachres : res) {
+            int x = eachres.get(0);
+            int y = eachres.get(1);
+            int pob = eachres.get(2);
+            // a pillar
             if (pob == 0) {
                 boolean check = false;
                 if (y == 0) check = true;
 
+                for (List<Integer> j : res) {
+                    // There is a beam at (x-1, y)
+                    if (x - 1 == j.get(0) && y == j.get(1) && 1 == j.get(2))
+                        check = true;
+                    // There is a beam at (x, y)
+                    if (x == j.get(0) && y == j.get(1) && 1 == j.get(2))
+                        check = true;
+                    // There is a pillar at (x, y-1)
+                    if (x == j.get(0) && y - 1 == j.get(1) && 0 == j.get(2))
+                        check = true;
+                }
+
+                if (!check) return false;
+            }
+            // a beam
+            else if (pob == 1) {
+                boolean check = false;
+                boolean left = false;
+                boolean right = false;
+
+                for (List<Integer> j : res) {
+                    if (x == j.get(0) && y - 1 == j.get(1) && 0 == j.get(2))
+                        check = true;
+                    if (x + 1 == j.get(0) && y - 1 == j.get(1) && 0 == j.get(2))
+                        check = true;
+                    if (x - 1 == j.get(0) && y == j.get(1) && 1 == j.get(2))
+                        left = true;
+                    if (x + 1 == j.get(0) && y == j.get(1) && 1 == j.get(2))
+                        right = true;
+                }
+
+                if (left && right) check = true;
+                if (!check) return false;
             }
         }
-
+        return true;
     }
 
     public int[][] solution(int n, int[][] build_frame) {
@@ -90,9 +119,29 @@ public class Q12 {
                 res.remove(ind);
                 if (!isPossibleToOperate(res))
                     res.add(erased);
+            } else if (oper == 1) {
+                List<Integer> inserted = new ArrayList<>();
+                inserted.add(x);
+                inserted.add(y);
+                inserted.add(pob);
+                res.add(inserted);
+                if (!isPossibleToOperate(res))
+                    res.remove(res.size() - 1);
             }
         }
+
+        List<Entry> resultList = new ArrayList<>();
+        for (List<Integer> i : res)
+            resultList.add(new Entry(i.get(0), i.get(1), i.get(2)));
+        Collections.sort(resultList);
+
+        int[][] result = new int[resultList.size()][3];
+        for (int i = 0; i < resultList.size(); ++i) {
+            result[i][0] = resultList.get(i).x;
+            result[i][1] = resultList.get(i).y;
+            result[i][2] = resultList.get(i).pob;
+        }
+        return result;
     }
 
 }
-*/
