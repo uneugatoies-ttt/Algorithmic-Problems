703bec3863dff4ee30eec5e4f9811ff35cf72aea
ttt Uneugatoies <t3u33t333@gmail.com>
2024-03-28 19:05:11 +0900

    commit 28

---
+++ java/src/ict_practice_revisit/Q34.java
@@ -0,0 +1,21 @@
+package ict_practice_revisit;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+    failed...
+*/
+
+
+public class Q34 {
+
+    private static void solve() throws IOException {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        solve();
+    }
+
+}
