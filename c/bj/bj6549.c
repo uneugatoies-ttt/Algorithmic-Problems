2729ef1125a504de832faf4545fa082cec752f4e
ttt Uneugatoies <t3u33t333@gmail.com>
2024-04-16 10:06:50 +0900

    Add a C solution for BJ6549 - following the divide and conquer paradigm

--- c/bj/bj6549.c
+++ c/bj/bj6549.c
@@ -1,77 +1,96 @@
+// Solution 1: following the divide and conquer "paradigm".
+
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-// This one keeps failing; but I don't know what is wrong. IWAAIL.
-long long getAreaConsecutive(int low, int high, int mid, long long* histogram) {
-	int leftward = mid;
-	int rightward = mid;
-	long long height = histogram[mid];
-	long long max = histogram[mid];
-	
-	int temp;
-	while (low < leftward && rightward < high) {
-		if (histogram[rightward + 1] > histogram[leftward - 1]) {
-			rightward++;
-			if (histogram[rightward] < height)
-				height = histogram[rightward];
-		} else {
-			leftward--;
-			if (histogram[leftward] < height)
-				height = histogram[leftward];
-		}
-		temp = height * ((long long) rightward - leftward + 1);
-		max = (max > temp) ? max : temp;
-	}
-
-	while (high > rightward) {
-		rightward++;
-		if (histogram[rightward] < height)
-			height = histogram[rightward];
-		temp = height * ((long long) rightward - leftward + 1);
-		max = (max > temp) ? max : temp;
-	}
-
-	while (low < leftward) {
-		leftward--;
-		if (histogram[leftward] < height)
-			height = histogram[leftward];
-		temp = height * ((long long) rightward - leftward + 1);
-		max = (max > temp) ? max : temp;
-	}
-
-
-	return max;
-}
-
-long long getArea(int low, int high, long long* histogram) {
-	if (low == high) return histogram[low];
-
-	int mid = (low + high) / 2;
-
-	long long left = getArea(low, mid, histogram);
-	long long right = getArea(mid + 1, high, histogram);
-	long long whole = getAreaConsecutive(low, high, mid, histogram);
-
-	long long max;
-	max = (left > right) ? left : right;
-	max = (max > whole) ? max : whole;
-
-	return max;
-}
+int* histogram;
+int n;
+long long getMaxArea(int low, int high);
+long long getMaxAreaConsecutive(int low, int mid, int high);
+long long getmx(long long x, long long y);
+long long getmn(long long x, long long y);
 
 int main() {
-	long long histogram[100001];
-
-	int n;
-	scanf("%d", &n);
-	while (n != 0) {
-		for (int i = 0; i < n; ++i)
-			scanf("%lld", &histogram[i]);
+	histogram = (int*)malloc(sizeof(int) * 100001);
+	long long m;
 	
-		printf("%lld\n", getArea(0, n - 1, histogram));
-	
+	while (1) {
 		scanf("%d", &n);
+		if (n == 0) break;
+
+		for (int i = 0; i < n; ++i)
+			scanf("%d", &histogram[i]);
+		m = getMaxArea(0, n - 1);
+		printf("%lld\n", m);
 	}
 
+	free(histogram);
+
 	return 0;
+}
+
+long long getmx(long long x, long long y) {
+	return (x > y) ? x : y;
+}
+
+long long getmn(long long x, long long y) {
+	return (x < y) ? x : y;
+}
+
+long long getMaxArea(int low, int high) {
+	int mid;
+	mid = (low + high) / 2;
+	if (low == high) return histogram[low];
+
+	long long left = getMaxArea(low, mid);
+	long long right = getMaxArea(mid + 1, high);
+	long long whole = getMaxAreaConsecutive(low, mid, high);
+
+	long long mx = getmx(whole, getmx(left, right));
+
+	return mx;
+}
+
+long long getMaxAreaConsecutive(int low, int mid, int high) {
+	long long len = 0;
+	long long leftward = mid;
+	long long leftval = histogram[leftward];
+	long long rightward = mid + 1;
+	long long rightval = histogram[rightward];
+	long long result = 2 * getmn(leftval, rightval);
+
+	while (low < leftward && rightward < high) {
+		if (leftval <= rightval) {
+			if (rightval <= histogram[++rightward]) {
+				result = getmx(rightval * (rightward - leftward), getmx(result, leftval * (rightward - leftward + 1)));
+			} else {
+				rightval = histogram[rightward];
+				result = getmx(result, getmn(rightval, leftval) * (rightward - leftward + 1));
+			}
+		} else {
+			if (leftval <= histogram[--leftward]) {
+				result = getmx(leftval * (rightward - leftward), getmx(result, rightval * (rightward - leftward + 1)));
+			} else {
+				leftval = histogram[leftward];
+				result = getmx(result, getmn(rightval, leftval) * (rightward - leftward + 1));
+			}
+		}
+	}
+	
+	while (low < leftward) {
+		leftval = getmn(leftval, histogram[--leftward]);
+		if (leftval <= rightval)
+			result = getmx(result, leftval * (rightward - leftward + 1));
+		else
+			result = getmx(result, getmx(rightval * (rightward - leftward + 1), leftval * (rightward - leftward)));
+	}
+	while (rightward < high) {
+		rightval = getmn(rightval, histogram[++rightward]);
+		if (rightval <= leftval)
+			result = getmx(result, rightval * (rightward - leftward + 1));
+		else
+			result = getmx(result, getmx(leftval * (rightward - leftward + 1), rightval * (rightward - leftward)));
+	}
+
+	return result;
 }
